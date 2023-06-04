e3476e25625a89dcd40406319683bbc53b897a8c
xhagrg <soulfreedom.3@gmail.com>
2023-06-04 18:14:02 -0500

    Update to make sure we do not run out of tokens.

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ import streamlit as st
 
 # Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
+# Import chroma as the vector store
 from langchain.vectorstores import Chroma
 
+# Import Document Schema
+from langchain.schema import Document
 # Import vector store stuff
 from langchain.agents.agent_toolkits import (
     create_vectorstore_agent,
@@ -19,22 +21,42 @@ from langchain.agents.agent_toolkits import (
 
 # Set APIkey for OpenAI Service
 # Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = 'yourapikeyhere'
+os.environ['OPENAI_API_KEY'] = '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'
 
 # Create instance of OpenAI LLM
 llm = OpenAI(temperature=0.1, verbose=True)
 
 # Create and load PDF Loader
-loader = PyPDFLoader('annualreport.pdf')
-# Split pages from pdf 
+file_name = "/Users/xhagrg/Downloads/IGARSS_Blockchain.pdf"
+loader = PyPDFLoader(file_name)
+
+# Split pages from pdf
 pages = loader.load_and_split()
+updated_pages = list()
+
+split_number = 0
+page_splits = 5
+
+for index, page in enumerate(pages):
+    single_page_length = int(len(page.page_content) / page_splits)
+    for split in range(1, page_splits):
+        updated_pages.append(Document(page_content=page.page_content[
+                ((split - 1) * single_page_length):(split * single_page_length)
+            ],
+            metadata={
+                'source': file_name,
+                'page': split_number
+            })
+        )
+        split_number += 1
 # Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, collection_name='annualreport')
+collection_name = 'document'
+store = Chroma.from_documents(updated_pages, collection_name=collection_name)
 
 # Create vectorstore info object - metadata repo?
 vectorstore_info = VectorStoreInfo(
-    name="annual_report",
-    description="a banking annual report as a pdf",
+    name=collection_name,
+    description="a document as a pdf",
     vectorstore=store
 )
 # Convert the document store into a langchain toolkit
@@ -46,7 +68,7 @@ agent_executor = create_vectorstore_agent(
     toolkit=toolkit,
     verbose=True
 )
-st.title('🦜🔗 GPT Investment Banker')
+st.title('Question and Answer based on a document')
 # Create a text input box for the user
 prompt = st.text_input('Input your prompt here')
 
@@ -57,9 +79,9 @@ if prompt:
     # ...and write it out to the screen
     st.write(response)
 
-    # With a streamlit expander  
-    with st.expander('Document Similarity Search'):
+    # With a streamlit expander
+    with st.expander('Context'):
         # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content) 
+        search = store.similarity_search_with_score(prompt)
+        # Write out the first
+        st.write(search[0][0].page_content)
